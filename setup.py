2d1a75535f6c89b0b74d23c452ed487e21b95396
Matt Odden <mrodden@us.ibm.com>
2017-01-28 14:24:42 -0600

    Add setup.py to make speyer installable with pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='speyer',
+    version='0.1.0',
+    auther='Mathew Odden',
+    auther_email='locke105@gmail.com',
+    install_requires=[
+        'requests',
+        'paramiko'
+    ]
+)
