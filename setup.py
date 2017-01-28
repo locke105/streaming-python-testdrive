a7121354b51cc6ff94c49877bb505d51423c2ef2
Matt Odden <mrodden@us.ibm.com>
2017-01-28 14:40:30 -0600

    Add speyer package to setup.py :)

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     version='0.1.0',
     auther='Mathew Odden',
     auther_email='locke105@gmail.com',
+    packages=setuptools.find_packages(),
     install_requires=[
         'requests',
         'paramiko'
